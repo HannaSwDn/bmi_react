e481cee133f03e4019c372804b632cc3fdd5c7de
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-07 15:55:10 +0100

    Adds constructor to App class, sets state of weight and height to a string

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: 'enter your weight',
+      height: 'enter your height'
+    }
+  }
+
   render() {
     return (
       <div className="App">
