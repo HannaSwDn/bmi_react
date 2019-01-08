40af0e7214a817285b0208ba159a63a6ef989adc
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-08 10:07:20 +0100

    Creates if / else statement instead of many if statements

--- src/BMICalculator.js
+++ src/BMICalculator.js
@@ -19,18 +19,12 @@ export const bmiCalculation = (weight, height, method) => {
 
 const setBMIMessage = (finalBMI) => {
   if (finalBMI < 18.5) {
-    return "Underweight";
-  }
-
-  if (finalBMI > 18.5 && finalBMI < 25) {
-    return "Normal";
-  }
-
-  if (finalBMI > 25 && finalBMI < 30) {
-    return "Overweight";
-  }
-
-  if (finalBMI > 30) {
-    return "Obese";
+    return 'Underweight';
+  } else if (finalBMI > 18.5 && finalBMI < 25) {
+    return 'Normal';
+  } else if (finalBMI > 25 && finalBMI < 30) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
   }
 }
